17e80c04614d2345561657178af7099561b99940
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2022-11-06 17:49:52 +0100

    fix(frontend | Page): 🐛 improved rounded header design

--- packages/frontend/src/components/Page.tsx
+++ packages/frontend/src/components/Page.tsx
@@ -3,14 +3,11 @@ import Header from './Header';
 
 const Page = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="flex h-screen bg-indigo-900">
+    <div className="flex h-screen bg-gray-900">
       <div className="flex flex-1 flex-col overflow-hidden align-middle">
         <Header />
-
-        <div className="absolute top-14 flex h-6 w-full rounded-t-full bg-indigo-900"></div>
-        <div className="flex h-full">
+        <div className="flex h-full rounded-t-3xl bg-blue-900 pt-5">
           <main className="flex w-full flex-col overflow-y-auto  overflow-x-hidden ">
-            <div className="flex h-6 w-full bg-gray-900" />
             <div className="mx-auto flex w-11/12">{children}</div>
           </main>
         </div>
