dbe5574b8336b11a39d82a80ed374afdeb76d5dd
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2022-11-06 10:43:26 +0100

    feat(api | database): 🗃️ connect database to nest's app module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+      {
+        dbName: process.env.MONGO_DATABASE,
+        auth: {
+          username: process.env.MONGO_USERNAME,
+          password: process.env.MONGO_PASSWORD,
+        },
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
